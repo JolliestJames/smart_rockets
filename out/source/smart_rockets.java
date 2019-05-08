b5234be43edf9fcf04d492a805b65c3171813e1f
JolliestJames <cloudismydog@gmail.com>
2019-05-08 13:54:26 -0700

    Update out/

--- out/source/smart_rockets.java
+++ out/source/smart_rockets.java
@@ -14,44 +14,205 @@ import java.io.IOException;
 
 public class smart_rockets extends PApplet {
 
-Rocket rocket;
+int lifeCounter;
+Population population;
+
+final int LIFETIME = 360;
+
+PVector target;
 
 public void setup() {
   
-  rocket = new Rocket(new PVector(100, 100));
+  float mutationRate = 0.01f;
+  lifeCounter = 0;
+  target = new PVector(width/2, 24);
+  population = new Population(mutationRate, 50);
 }
 
 public void draw() {
   background(255);
-  rocket.update();
-  rocket.display();
-}
+  fill(0);
+  ellipse(target.x, target.y, 24, 24);
 
-public void keyPressed() {
-  if (keyCode == RIGHT) {
-    rocket.applyForce(new PVector(10, 0));
+  if (lifeCounter < LIFETIME) {
+    population.live();
+    lifeCounter++;
   } else {
-    rocket.applyForce(new PVector(-10, 0));
+    lifeCounter = 0;
+    population.fitness();
+    population.selection();
+    population.reproduction();
+  }
+
+  fill(0);
+  text("Generation #: " + population.generations(), 10, 18);
+  text("Cycles left: " + (LIFETIME-lifeCounter), 10, 36);
+}
+class DNA {
+  PVector[] genes;
+  float maxForce = 0.1f;
+
+  DNA() {
+    genes = new PVector[LIFETIME];
+
+    for(int i = 0; i < genes.length; i++) {
+      genes[i] = PVector.random2D();
+      genes[i].mult(random(0, maxForce));
+    }
+  }
+
+  DNA(PVector[] old) {
+    genes = old;
+  }
+
+  public DNA crossover(DNA partner) {
+    DNA child = new DNA();
+
+    for (int i = 0; i < genes.length; i++) {
+      int coinToss = PApplet.parseInt(random(2));
+      if (coinToss == 1) {
+        child.genes[i] = genes[i];
+      } else {
+        child.genes[i] = partner.genes[i];
+      }
+    }
+
+    return child;
+  }
+
+  public void mutate(float mutationRate) {
+    for (int i = 0; i < genes.length; i++) {
+      if (random(1) < mutationRate) {
+        float angle = random(TWO_PI);
+        genes[i] = new PVector(cos(angle), sin(angle));
+        genes[i].mult(random(0, maxForce));
+      }
+    }
   }
 }
+class Population {
+  float mutationRate;
+  Rocket[] population;
+  ArrayList<Rocket> matingPool;
+  int generations;
 
+  Population(float mRate, int totalPopulation) {
+    mutationRate = mRate;
+    population = new Rocket[totalPopulation];
+    matingPool = new ArrayList<Rocket>();
+    generations = 0;
+    for (int i = 0; i < population.length; i++) {
+      PVector position = new PVector(width/2,height+20);
+      population[i] = new Rocket(position, new DNA());
+    }
+  }
+
+  public void live() {
+    for(int i = 0; i < population.length; i++) {
+      population[i].run();
+    }
+  }
+
+  public void fitness() {
+    for (int i = 0; i < population.length; i++) {
+      population[i].fitness();
+    }
+  }
+  
+  public void selection() {
+    matingPool.clear();
+
+    float max = maxFitness();
+
+    for (int i = 0; i < population.length; i++) {
+      float fitnessNormal = map(population[i].fitness(), 0, max, 0, 1);
+      int n = (int) (fitnessNormal * 100);
+      for (int j = 0; j < n; j++) {
+        matingPool.add(population[i]);
+      }
+    }
+  }
+
+  public void reproduction() {
+    for (int i = 0; i < population.length; i++) {
+      int a = PApplet.parseInt(random(matingPool.size()));
+      int b = PApplet.parseInt(random(matingPool.size()));
+      Rocket partnerA = matingPool.get(a);
+      Rocket partnerB = matingPool.get(b);
+      DNA aDNA = partnerA.dna(); 
+      DNA bDNA = partnerB.dna();
+      DNA child = aDNA.crossover(bDNA);
+      child.mutate(mutationRate);
+      PVector position = new PVector(width/2, height+20);
+  
+      population[i] = new Rocket(position, child);
+    }
+    generations++;
+  }
+
+  public int generations() {
+    return generations;
+  }
+
+  public float maxFitness() {
+    float max = 0;
+
+    for (int i = 0; i < population.length; i++) {
+       if(population[i].fitness() > max) {
+         max = population[i].fitness();
+       }
+    }
+
+    return max;
+  }
+}
 class Rocket {
 
   PVector position;
   PVector velocity;
   PVector acceleration;
 
-  float size;
+  DNA dna;
 
-  Rocket(PVector location) {
+  float size;
+  float fitness;
+  int geneCounter = 0;
+
+  Rocket(PVector location, DNA _dna) {
     acceleration = new PVector();
     velocity = new PVector();
     position = location.get();
     size = 4;
+    dna = _dna;
+  }
+
+  public void run() {
+    if (!reachedTarget()) {
+      applyForce(dna.genes[geneCounter]);
+      geneCounter = (geneCounter + 1) % dna.genes.length;
+      update();
+    }
+    display();
+  }
+
+  public float fitness() {
+    float distance = dist(position.x, position.y, target.x, target.y);
+    fitness = pow(1.0f / distance, 2);
+    return fitness;
   }
 
   public void applyForce(PVector f) {
     acceleration.add(f);
+  }
+
+  public boolean reachedTarget() {
+    float distance = dist(position.x, position.y, target.x, target.y);
+
+    if (distance < 12) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   public void update() {
@@ -85,8 +246,11 @@ class Rocket {
     popMatrix();
   }
 
+  public DNA dna() {
+    return dna;
+  }
 }
-  public void settings() {  size(800, 600); }
+  public void settings() {  size(640, 360); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "smart_rockets" };
     if (passedArgs != null) {
